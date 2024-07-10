8e9e4527381c38db22b338f1af7421725eb681cb
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-10 23:35:24 +0800

    feat: add routes for get and post method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { todoRouter } from './routes/todo';
 
-const app = express()
-app.use(json())
+const app = express();
+app.use(json());
+app.use(todoRouter);
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
