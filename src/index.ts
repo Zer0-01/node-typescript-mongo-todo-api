d77b63d7d097d87306050dadd1a748f2bc0daf00
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-11 23:21:52 +0800

    feat: connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { json } from 'body-parser';
 import { todoRouter } from './routes/todo';
 
 const app = express();
 app.use(json());
 app.use(todoRouter);
+
+mongoose.connect('mongodb://localhost:27017/todo')
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((error) => {
+        console.error('Error connecting to database: ', error);
+    });
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
